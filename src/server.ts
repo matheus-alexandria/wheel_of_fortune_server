f57f9e916b46cda9511413ef9efff23fb3c5130a
Matchobas <maalexandria@hotmail.com>
2023-04-14 16:29:58 -0300

    Adding env variables with dotenv

--- src/server.ts
+++ src/server.ts
@@ -1,22 +1,19 @@
 import express from "express";
 import cors from "cors";
 import { createObjectCsvWriter } from "csv-writer";
+import "dotenv/config";
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.get("/", (request, response) => {
-  console.log("Route");
-  response.send();
-});
-
 app.post("/", (request, response) => {
   const { name, options } = request.body;
+  const path = process.env.CSV_FILES_PATH || '';
   const fileName = `${name}.csv`;
 
   const csvWriter = createObjectCsvWriter({
-    path: `/home/matheus/Documents/projetos/rouletteOnline/server/temp/${fileName}`,
+    path: `${path}${fileName}`,
     header: [
       { id: 'title', title: 'Title'},
       { id: 'weight', title: 'Weight'},
